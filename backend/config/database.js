02aee91cebdf758876381800fc31ef42950c8a69
achrafameur <achref.ameur1925@gmail.com>
2024-02-02 15:36:45 +0100

    ajout de standardjs

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
-
+const mongoose = require('mongoose')
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    }).then(con => {
-        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`)
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    .then((con) => {
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      )
     })
 }
 
