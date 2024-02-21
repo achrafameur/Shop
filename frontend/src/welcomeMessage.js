0b0304f2d7e1471233c1378dbd06837c19789f4d
achrafameur <achref.ameur1925@gmail.com>
2024-02-21 09:00:42 +0100

    ajout des tests avec jest

---
+++ frontend/src/welcomeMessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelcomeMessage = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur notre application !</h1>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
